c02bfdaf90f3aa4a16e213baaec5b7338010e4de
Kar2831 <karthikeyanm282006@gmail.com>
2025-01-06 14:08:34 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import {Menu,X} from "lucide-react";
+import { useState } from "react";
+import React from 'react'
+import logo from '../assets/logo.png'
+import { navItems } from '../constants/index';
+const Navbar = () => {
+
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+    const toggelMobileDrawer = () => {
+        setMobileDrawerOpen(!mobileDrawerOpen);
+    }
+
+  return (
+        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg borde-b border-neutral-700/80">
+            <div className="container px-4 mx-auto relative text-sm">
+                <div className='flex justify-between items-center'>
+                    <div className="flex items-center flex-shrink-0">
+                        <img className='w-10 h-10 mr-2' src={logo} alt='logo' />
+                        <span className="text-xl tracking-tight">VirtualR</span>
+                    </div>
+                    <ul className='hidden lg:flex ml-14 space-x-12'> {
+                                navItems.map((item,index)=>(
+                                    <li>
+                                        <a>{item.label}</a>
+                                    </li>
+                                ))
+                            }
+                    </ul>
+                    <div className="hidden lg:flex justify-center space-x-12 items-center">
+                        <a href='#' className='py-2 px-3 border rounded-md'>
+                            Sign In
+                        </a>
+                        <a href='#' className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'>Create an account</a>
+                    </div>
+                    <div className="lg:hidden flex flex-col justify-center items-center">
+                        <button onClick={toggelMobileDrawer}>{mobileDrawerOpen ? <X/> : <Menu/>}</button>
+                    </div>
+                </div>
+                {mobileDrawerOpen &&  <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                           <ul className="lg:hidden flex flex-col justify-center items-center">
+                        {
+                            navItems.map((item,index)=>(
+                                <li key={index} className="py-4">
+                                    <a href={item.href} >{item.label}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                            <div className="flex justify-center items-center gap-10">
+                            <a href='#' className='py-2 px-3 border border-gray-400 rounded-md hover:border-white'>
+                            Sign In
+                        </a>
+                        <a href='#' className='bg-gradient-to-r from-orange-500 to-orange-600 hover:bg-gradient-to-r hover:from-orange-600 hover:to-orange-500 py-2 px-3 rounded-md'>Create an account</a>
+                            </div>
+                </div>}
+            </div>
+        </nav>
+)
+}
+
+export default Navbar
